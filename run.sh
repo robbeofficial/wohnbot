3aaa01caac941a4930cd23d34cfdd2464ad578ed
Robert Walter <609855+robbeofficial@users.noreply.github.com>
2024-08-16 15:52:12 +0200

    fix run script

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-source venv/bin/activate
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+VENV_DIR="${SCRIPT_DIR}/venv"
+
+source "${VENV_DIR}/bin/activate"
 python -m wohnbot.main
