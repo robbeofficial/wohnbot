bf0aa6dbf1349ec99706b57a704fac8c1e6b7886
Robert Walter <609855+robbeofficial@users.noreply.github.com>
2025-02-20 20:30:53 +0100

    avoid concurrent instances of run script

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 VENV_DIR="${SCRIPT_DIR}/venv"
+
+# Avoid running multiple instances of the script
+exec 200>/tmp/wohnbot.lock  # Open /tmp/wohnbot.lock and assign it to FD 200
+flock -n 200 || { echo "Job is already running"; exit 1; }  # Try to lock FD 200
 
 cd "${SCRIPT_DIR}"
 source "${VENV_DIR}/bin/activate"
