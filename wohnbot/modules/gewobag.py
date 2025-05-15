427a5fc60889e1482fe918c55337c395fbbfe6e7
Robert Walter <609855+robbeofficial@users.noreply.github.com>
2025-05-15 17:49:13 +0200

    update gewobag

--- wohnbot/modules/gewobag.py
+++ wohnbot/modules/gewobag.py
@@ -13,86 +13,46 @@ def found(response):
 
 
 def scrape(session):
+    import requests
+
     cookies = {
-        'borlabs-cookie': '%7B%22consents%22%3A%7B%22essential%22%3A%5B%22borlabs-cookie%22%2C%22accessibility_contrast%22%2C%22accessibility_test_size%22%2C%22language_switch%22%2C%22location_agreement%22%5D%2C%22statistics%22%3A%5B%22matomo%22%5D%2C%22external-media%22%3A%5B%22gewobag-youtube%22%2C%22googlemaps%22%5D%7D%2C%22domainPath%22%3A%22www.gewobag.de%2F%22%2C%22expires%22%3A%22Fri%2C%2006%20Oct%202023%2017%3A56%3A09%20GMT%22%2C%22uid%22%3A%22g502zxlo-73pjh7xi-zuj2rk6a-ewv9i5af%22%2C%22version%22%3A%223%22%7D',
+        'borlabs-cookie': '%7B%22consents%22%3A%7B%22essential%22%3A%5B%22borlabs-cookie%22%2C%22accessibility_contrast%22%2C%22accessibility_test_size%22%2C%22location_agreement%22%5D%7D%2C%22domainPath%22%3A%22www.gewobag.de%2F%22%2C%22expires%22%3A%22Mon%2C%2018%20Aug%202025%2015%3A02%3A31%20GMT%22%2C%22uid%22%3A%22anonymous%22%2C%22v3%22%3Atrue%2C%22version%22%3A2%7D',
     }
 
     headers = {
-        'authority': 'www.gewobag.de',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'accept-language': 'en-DE,en-US;q=0.9,en;q=0.8,de-DE;q=0.7,de;q=0.6,en-GB;q=0.5',
-        'cache-control': 'no-cache',
-        # 'cookie': 'borlabs-cookie=%7B%22consents%22%3A%7B%22essential%22%3A%5B%22borlabs-cookie%22%2C%22accessibility_contrast%22%2C%22accessibility_test_size%22%2C%22language_switch%22%2C%22location_agreement%22%5D%2C%22statistics%22%3A%5B%22matomo%22%5D%2C%22external-media%22%3A%5B%22gewobag-youtube%22%2C%22googlemaps%22%5D%7D%2C%22domainPath%22%3A%22www.gewobag.de%2F%22%2C%22expires%22%3A%22Fri%2C%2006%20Oct%202023%2017%3A56%3A09%20GMT%22%2C%22uid%22%3A%22g502zxlo-73pjh7xi-zuj2rk6a-ewv9i5af%22%2C%22version%22%3A%223%22%7D',
-        'pragma': 'no-cache',
-        'referer': 'https://www.gewobag.de/fuer-mieter-und-mietinteressenten/mietangebote/',
-        'sec-ch-ua': '"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Linux"',
-        'sec-fetch-dest': 'document',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-user': '?1',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'de,en-US;q=0.7,en;q=0.3',
+        # 'Accept-Encoding': 'gzip, deflate, br, zstd',
+        'Referer': 'https://www.gewobag.de/fuer-mietinteressentinnen/mietangebote/',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        # 'Cookie': 'borlabs-cookie=%7B%22consents%22%3A%7B%22essential%22%3A%5B%22borlabs-cookie%22%2C%22accessibility_contrast%22%2C%22accessibility_test_size%22%2C%22location_agreement%22%5D%7D%2C%22domainPath%22%3A%22www.gewobag.de%2F%22%2C%22expires%22%3A%22Mon%2C%2018%20Aug%202025%2015%3A02%3A31%20GMT%22%2C%22uid%22%3A%22anonymous%22%2C%22v3%22%3Atrue%2C%22version%22%3A2%7D',
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-User': '?1',
+        'Priority': 'u=0, i',
     }
 
     params = {
-        'bezirke_all': '1',
-        'bezirke[]': [
-            'charlottenburg-wilmersdorf',
-            'charlottenburg-wilmersdorf-charlottenburg',
-            'friedrichshain-kreuzberg',
-            'friedrichshain-kreuzberg-friedrichshain',
-            'friedrichshain-kreuzberg-kreuzberg',
-            'lichtenberg',
-            'lichtenberg-alt-hohenschoenhausen',
-            'lichtenberg-falkenberg',
-            'lichtenberg-fennpfuhl',
-            'marzahn-hellersdorf',
-            'marzahn-hellersdorf-marzahn',
-            'mitte',
-            'mitte-gesundbrunnen',
-            'mitte-moabit',
-            'mitte-wedding',
-            'neukoelln',
-            'neukoelln-britz',
-            'neukoelln-buckow',
-            'neukoelln-rudow',
-            'pankow',
-            'pankow-prenzlauer-berg',
-            'reinickendorf',
-            'reinickendorf-hermsdorf',
-            'reinickendorf-tegel',
-            'reinickendorf-waidmannslust',
-            'spandau',
-            'spandau-hakenfelde',
-            'spandau-haselhorst',
-            'spandau-staaken',
-            'spandau-wilhelmstadt',
-            'steglitz-zehlendorf',
-            'steglitz-zehlendorf-lichterfelde',
-            'steglitz-zehlendorf-steglitz',
-            'tempelhof-schoeneberg',
-            'tempelhof-schoeneberg-lichtenrade',
-            'tempelhof-schoeneberg-mariendorf',
-            'tempelhof-schoeneberg-marienfelde',
-            'tempelhof-schoeneberg-schoeneberg',
-            'treptow-koepenick',
-            'treptow-koepenick-altglienicke',
-            'treptow-koepenick-niederschoeneweide',
-        ],
-        'nutzungsarten[]': 'wohnung',
+        'objekttyp[]': 'wohnung',
         'gesamtmiete_von': '',
         'gesamtmiete_bis': '',
         'gesamtflaeche_von': '',
         'gesamtflaeche_bis': '',
         'zimmer_von': '',
         'zimmer_bis': '',
-        'sort-by': 'recent',
+        'sort-by': '',
     }
 
-    response = session.get('https://www.gewobag.de/fuer-mieter-und-mietinteressenten/mietangebote/',
-                            params=params, cookies=cookies, headers=headers, timeout=wohnbot.params['scraping']['timeout'])
+    response = requests.get(
+        'https://www.gewobag.de/fuer-mietinteressentinnen/mietangebote/',
+        params=params,
+        # cookies=cookies,
+        headers=headers,
+    )
     return response.text
 
 
