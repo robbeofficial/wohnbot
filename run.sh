63ff5ef0d89646a9f1fd1892001b8c272786436f
Robert Walter <609855+robbeofficial@users.noreply.github.com>
2026-03-11 15:42:25 +0100

    prevent from never terminatiing

--- run.sh
+++ run.sh
@@ -4,8 +4,14 @@ VENV_DIR="${SCRIPT_DIR}/venv"
 
 # Avoid running multiple instances of the script
 exec 200>/tmp/wohnbot.lock  # Open /tmp/wohnbot.lock and assign it to FD 200
-flock -n 200 || { echo "Job is already running"; exit 1; }  # Try to lock FD 200
+flock -n 200 || { echo "$(date '+%Y-%m-%d %H:%M:%S') - Job is already running"; exit 1; }  # Try to lock FD 200
 
 cd "${SCRIPT_DIR}"
 source "${VENV_DIR}/bin/activate"
-python -m wohnbot.main
+timeout 10m python -m wohnbot.main
+EXIT_CODE=$?
+
+# 4. Catch the timeout exit code (124) and log it
+if [ $EXIT_CODE -eq 124 ]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Python script timed out after 10m and was killed."
+fi
